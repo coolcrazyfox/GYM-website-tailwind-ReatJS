f4cddda27104a4128d20b185576edc5ee1d37bbe
Andrei Malets <coolviktori@yandex.by>
2024-11-23 23:51:34 +0300

    changes NavBar

--- src/components/Navbar/NavBar.jsx
+++ src/components/Navbar/NavBar.jsx
@@ -1,7 +1,24 @@
-import { FaSearch } from "react-icons/fa";
+import { FaSearch, FaDumbbell, FaCartArrowDown } from "react-icons/fa";
+import { CgMenuGridR } from "react-icons/cg";
 
 const NavBar = () => {
-  return <div>navbar</div>;
+  return (
+    <>
+      <nav>
+        <div className="container">
+          <div>
+            <FaDumbbell />
+            <p>Main</p>
+            <p>Gym</p>
+          </div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <FaSearch />
+      </nav>
+    </>
+  );
 };
 
 export default NavBar;

1c4a9908486be6670bce55b3a331c376ce85f4ef
Andrei Malets <coolviktori@yandex.by>
2024-11-24 00:08:13 +0300

    changes NavBar

--- src/components/Navbar/NavBar.jsx
+++ src/components/Navbar/NavBar.jsx
@@ -6,21 +6,21 @@ const NavBar = () => {
   return (
     <>
       <nav>
-        <div className="container">
+        <div className="container flex justify-between items-center py-8">
           <div className="text-2xl flex items-center gap-2 fon-bold py-8 uppercase select-none">
             <FaDumbbell className="cursor-pointer text-purple-300" />
             <p>Main</p>
             <p className="text-secondary">Gym</p>
           </div>
-          <div>
-            <ul>
+          <div className="hidden md:block">
+            <ul className="flex items-center gap6 text-purple-300">
               {NavbarMenu.map((item) => {
                 return (
                   <li
                     key={item.id}
                     className="uppercase cursor-pointer select-none"
                   >
-                    {item.title}
+                    <a href={item.link}>{item.title}</a>
                   </li>
                 );
               })}

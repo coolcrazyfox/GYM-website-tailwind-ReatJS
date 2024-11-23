e63661aa4648bf7699d43f5eddb2b7863fbf43a6
Andrei Malets <coolviktori@yandex.by>
2024-11-23 23:32:26 +0300

    create mockData

---
+++ src/mockData/data.js
@@ -0,0 +1,7 @@
+export const NavbarMenu = [
+  { id: 1, title: "Home", link: "/" },
+  { id: 2, title: "Trainer", link: "/trainer" },
+  { id: 3, title: "Program", link: "/program" },
+  { id: 4, title: "Blogs", link: "/blogs" },
+  { id: 5, title: "Pricing", link: "/pricing" },
+];

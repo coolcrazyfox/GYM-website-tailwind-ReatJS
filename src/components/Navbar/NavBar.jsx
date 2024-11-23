53fa51bfcb72b6d77dcf4d58a9205df47ca2bdfe
Andrei Malets <coolviktori@yandex.by>
2024-11-24 00:01:47 +0300

    changes NavBar

--- src/components/Navbar/NavBar.jsx
+++ src/components/Navbar/NavBar.jsx
@@ -1,21 +1,28 @@
 import { FaSearch, FaDumbbell, FaCartArrowDown } from "react-icons/fa";
 import { CgMenuGridR } from "react-icons/cg";
+import { NavbarMenu } from "../../mockData/data";
 
 const NavBar = () => {
   return (
     <>
       <nav>
         <div className="container">
-          <div className="text-2xl flex items-center gap-2 fon-bold py-8">
-            <FaDumbbell />
+          <div className="text-2xl flex items-center gap-2 fon-bold py-8 uppercase select-none">
+            <FaDumbbell className="cursor-pointer text-purple-300" />
             <p>Main</p>
-            <p>Gym</p>
+            <p className="text-secondary">Gym</p>
+          </div>
+          <div>
+            <ul>
+              {NavbarMenu.map((item) => {
+                return <li key={item.id}>{item.title}</li>;
+              })}
+            </ul>
           </div>
           <div></div>
           <div></div>
-          <div></div>
         </div>
-        <FaSearch />
+        {/* <FaSearch /> */}
       </nav>
     </>
   );

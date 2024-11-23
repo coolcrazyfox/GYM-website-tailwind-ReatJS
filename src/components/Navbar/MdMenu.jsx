75dcedb5688c4d9a19db0f5192379b5adc4eea57
Andrei Malets <coolviktori@yandex.by>
2024-11-24 01:17:28 +0300

    changes MdMenu

--- src/components/Navbar/MdMenu.jsx
+++ src/components/Navbar/MdMenu.jsx
@@ -10,12 +10,20 @@ const MdMenu = ({ opened }) => {
           initial={{ opacity: 0, y: -100 }}
           animate={{ opacity: 1, y: 0 }}
           exit={{ opacity: 0, y: -100 }}
+          transition={{ duration: 0.3 }}
           className="absolute top-20 left-0 w-full h-screen  z-20"
         >
           <div className="text-xl font-semibold uppercase bg-primary text-white py-10 m-6 rounded-3xl">
-            <ul className="flex flex-col justify-center items-center gap-10">
+            <ul className="flex flex-col justify-center items-center gap-10 select-none">
               {NavbarMenuMd.map((i) => {
-                return <li key={i.id}>{i.title}</li>;
+                return (
+                  <li
+                    key={i.id}
+                    className="cursor-pointer hover:text-purple-200"
+                  >
+                    {i.title}
+                  </li>
+                );
               })}
             </ul>
           </div>
